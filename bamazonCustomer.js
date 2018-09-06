aab0af738e158748290dc7c1ee582066a30a5f33
Kristin Herrmann <dickmann.michael@gmail.com>
2018-09-06 15:43:58 -0600

    updatedTable

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,31 @@
 var mysql = require("mysql");
+require("console.table");
+
 
 var connection = mysql.createConnection({
-    host: "localhost"
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "bamazondb",
 });
+
+connection.connect(function(err){
+    if (err) {
+        console.log("error" + err)
+    }
+    else {
+        console.log("working")
+        loadProduct();
+    }
+});
+
+//this will show a table of everything in the db
+function loadProduct(){
+connection.query("SELECT * FROM products", function(error, results){
+    if(error) throw error;
+    // console.log(results);
+    console.table(results);
+ 
+
+});
+}
