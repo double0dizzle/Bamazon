98d5ee7600c1999e95dc71b8fc55af0ec217c721
Kristin Herrmann <dickmann.michael@gmail.com>
2018-09-07 22:56:55 -0600

    prompt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 require("console.table");
+var inquirer = require("inquirer");
 
 
 var connection = mysql.createConnection({
@@ -16,8 +17,10 @@ connection.connect(function(err){
     else {
         console.log("working")
         loadProduct();
+        
     }
 });
+
 
 //this will show a table of everything in the db
 function loadProduct(){
@@ -25,7 +28,84 @@ connection.query("SELECT * FROM products", function(error, results){
     if(error) throw error;
     // console.log(results);
     console.table(results);
- 
-
-});
+ });
 }
+
+//search npm inquirer and look at greatBayBasic.js in the projects folder
+function firstPrompt() {
+    inquirer
+      .prompt([
+        {
+        name: "productID",
+        type: "list",
+        message: "Which Product ID would you like to purchase?",
+            choices: [
+              "1",
+              "2",
+              "3",
+              "4",
+              "5",
+              "6",
+              "7",
+              "8",
+              "9",
+              "10"
+            ]
+          },
+        {
+        name: "productQuantity",
+        type: "list",
+        message: "How many would you like?",
+        choices: [
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10"
+        ]
+      },
+      
+      ]).then(function(answers) {
+        //console.log(answers);
+        // connection.query("SELECT item_id FROM products", function(error, results){
+        //     if(error) throw error});
+        //     // console.log(error);
+            
+        //     switch (answers.productID) {
+        //            case "1":
+        //            quantity1();
+        //            break}
+        quantity1(answers);
+        
+      });
+  }
+
+   function quantity1(answers){
+       console.log(answers);
+       var quantity = parseInt(answers.productQuantity);
+       var ID = parseInt(answers.productID);
+       console.log(quantity);
+       if (answers.productQuantity <= 16) {
+        connection.query("UPDATE products SET stock_quantity = stock_quantity - " + quantity + "  WHERE item_id = " + ID , function(error, results){
+        console.log("update")
+        loadProduct();
+
+        })
+       } else {
+           console.log("Insufficient quantity!")
+       }
+   }
+   firstPrompt();
+
+//   .then(function(answer) {
+//     switch (answer.action) {
+//     case "Find songs by artist":
+//       artistSearch();
+//       break;
+
+
